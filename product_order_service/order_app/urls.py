987ae2bd460df26ed97f323526c6a1b2e3ba0bba
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2021-09-14 05:32:18 +1000

    logout added

--- product_order_service/order_app/urls.py
+++ product_order_service/order_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from order_app.views import ProductViewSet, UserViewSet, OrderViewSet, RegistrationViewSet
+from order_app.views import ProductViewSet, UserViewSet, OrderViewSet, RegistrationViewSet, TokenViewSet
 
 urlpatterns = format_suffix_patterns([
     path('products/', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
@@ -13,5 +13,7 @@ urlpatterns = format_suffix_patterns([
     path('user-info/', UserViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('user-info/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update'})),
     path('registration/', RegistrationViewSet.as_view({'post': 'create'})),
-    path('login/', views.obtain_auth_token)
+    path('login/', views.obtain_auth_token),
+    path('logout/<int:pk>/', TokenViewSet.as_view({'delete': 'destroy'}))
 ])
+
