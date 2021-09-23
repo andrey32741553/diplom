bc91e18dc0bd43c168b89690ec2fde46a8987d87
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2021-09-24 00:05:02 +0200

    djoser added

--- product_order_service/product_order_service/urls.py
+++ product_order_service/product_order_service/urls.py
@@ -4,4 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('order_app.urls'))]
+    path('api/v1/', include('order_app.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken'))
+]
