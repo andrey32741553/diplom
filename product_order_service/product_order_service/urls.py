02c684bba58ad5c6979bf7f817a4a5cacd5d7270
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2021-09-28 21:10:49 +0200

    social oauth added

--- product_order_service/product_order_service/urls.py
+++ product_order_service/product_order_service/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('order_app.urls')),
+    path('api/v1/auth/', include('rest_framework_social_oauth2.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken'))
 ]
