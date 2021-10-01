73a12e112d4bc414576fdb0698762dde07eb66c7
andrey32741553 <65930776+andrey32741553@users.noreply.github.com>
2021-10-01 21:59:28 +1000

    routes added for testing

--- product_order_service/product_order_service/urls.py
+++ product_order_service/product_order_service/urls.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+# from .spectacular import urlpatterns as doc_urls
+from rest_framework import routers
+
+from order_app.views import ProductViewSet, OrderViewSet
+from .yasg import urlpatterns as doc_urls
+
+
+router = routers.DefaultRouter()
+router.register("products", ProductViewSet, basename="products")
+router.register("orders", OrderViewSet, basename="orders")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('order_app.urls')),
+    path('api/v1/', include(router.urls)),
     path('api/v1/auth/', include('rest_framework_social_oauth2.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken'))
 ]
+
+urlpatterns += doc_urls
